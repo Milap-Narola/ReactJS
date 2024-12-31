10785ad2d480fe3f66b1e404f65f167c2d7bf9d9
Milap Narola <milap5322@gmail.com>
2024-12-31 15:24:24 +0530

    Add

---
+++ Lecture-2/counter/src/App.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  increment = () => {
+    this.setState(prevState => ({
+      count: prevState.count + 1
+    }));
+  }
+
+  decrement = () => {
+    this.setState(prevState => ({
+      count: prevState.count - 1
+    }));
+  }
+
+  render() {
+    return (
+      <div className="counter-container">
+      <div className="counter-content">
+        <h2>Counter: {this.state.count}</h2>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+      </div>
+    </div>
+    );
+  }
+}
+
+export default Counter;
