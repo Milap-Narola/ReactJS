a1ed616e38c9afe3bec84ef9d83eaa99aed6c97f
Milap <148769224+Milap-Narola@users.noreply.github.com>
2025-02-04 14:56:07 +0530

    Delete backend directory

--- backend/config/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-
-const connectDB =async ()=>{
-    await mongoose.connect(process.env.DB_URL)
-    console.log('Connected to MongoDB')
-}
-module.exports = connectDB
