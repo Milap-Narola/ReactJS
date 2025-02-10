774d4886e686dd4c0ca82237fa54857762347862
Milap Narola <milap5322@gmail.com>
2025-02-10 23:38:19 +0530

    Add

---
+++ Task_management/server/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const connectDb = require("./config/db");
+const appRoutes = require("./routes/index")
+require("dotenv").config();
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/v1",appRoutes)
+app.get("/", (req, res) => {
+  res.send("Welcome to the API");
+});
+const PORT = process.env.PORT || 8090;
+app.listen(PORT, () => {
+  console.log("listening on port PORT", PORT);
+  connectDb;
+});
