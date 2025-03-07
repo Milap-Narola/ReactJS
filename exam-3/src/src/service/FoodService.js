fa44fec7d2d498aaa9e8c7fef27109fa7d6f8659
Milap <148769224+Milap-Narola@users.noreply.github.com>
2025-03-07 19:29:19 +0530

    Delete exam-3/src/src directory

--- exam-3/src/src/service/FoodService.js
+++
@@ -1,7 +0,0 @@
-import API from "../config/Api";
-
-
-export const getFoods = () => API.get('/foods');
-export const createFood = (food) => API.post('/foods', food);
-export const updateFood = (id, food) => API.put(`/foods/${id}`, food);
-export const deleteFood = (id) => API.delete(`/foods/${id}`);
