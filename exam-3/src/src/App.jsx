157b0194f82c05e4ec56e786a370b9ad4db81588
Milap <148769224+Milap-Narola@users.noreply.github.com>
2025-03-07 19:18:50 +0530

    Add files via upload

---
+++ exam-3/src/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DataFood from './DataFood'
+
+const App = () => {
+  return (
+    <div className='main'>
+      <DataFood />
+    </div>
+  )
+}
+
+export default App
